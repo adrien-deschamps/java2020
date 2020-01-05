ba6275b973373bf64c51bdd69a1ce71ed7f908be
Katherine Zamudio-Turcotte <kathy_zt@yahoo.ca>
2020-01-05 11:48:44 -0500

    Ajout grimpeur
    Ajout classe sous-système grimpeur

---
+++ src/main/java/frc/robot/subsystems/Grimpeur.java
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Robot en 100h - Montréal                                                   */
+/* Nom : Grimpeur                                                             */
+/* Contributeurs : KatherineZT                                                */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Grimpeur extends SubsystemBase {
+  /**
+   * Creates a new ExampleSubsystem.
+   */
+  public Grimpeur() {
+      
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
